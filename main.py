c041faef78d577270e378978f720196210a00c67
Tejasva Khandelwal <khandelwaltejasva@gmail.com>
2024-03-15 16:09:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ from sql_connector import SQLConnector
 
 conn1 = SQLConnector('localhost', 'root', 'sqlconnect')
 conn1.connect_database('helloworld')
+print(conn1.see_databases())
+print(conn1.see_tables())
 
 conn1.close()
